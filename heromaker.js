e2c09bee00d0c590f1a85c2eb7b14faea9472d2b
hoviethung9 <hoviethung29032002@gmail.com>
2023-10-21 20:43:42 +0700

    as1

---
+++ heromaker.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAD3paI--G69OCHeEGD_UrBoM806pF04WA",
+    authDomain: "herroo.firebaseapp.com",
+    databaseURL: "https://herroo-default-rtdb.firebaseio.com",
+    projectId: "herroo",
+    storageBucket: "herroo.appspot.com",
+    messagingSenderId: "435793472033",
+    appId: "1:435793472033:web:e3ad604b7f5a1c44fcd160",
+    measurementId: "G-WN4E04H4N6"
+  };
